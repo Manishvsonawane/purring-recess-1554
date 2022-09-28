bc42c7cc41a8e5cec32fed4be36996d216434b31
gauravfar93 <gauravfar93@gmail.com>
2022-09-28 20:03:16 +0530

    added in fp05_010_day-2

---
+++ components/navbar.js
@@ -0,0 +1,22 @@
+function navbar (){
+   
+    return `<div id="navbar1">
+    <div id="left_part">
+        <p>WOMEN</p>
+        <p>MEN</p>
+        <p>KIDS</p>
+        <p>LIFE</p>
+    </div>
+    <div id="right_part">
+        <P>Signup for Newsletter</P>
+        <P>My account</P>
+        <P>My wishlist ♡</P>
+        <!-- <i class="fa-thin fa-heart"></i> -->
+        <P>Lebanon | English</P>
+    </div>
+
+</div>`
+
+
+}
+export default navbar;
