1c413125b0d7754916f4474e935038778a818058
1shubh <shubh3759@gmail.com>
2022-10-02 17:47:36 +0530

    added final repo

--- components/navbar.js
+++ components/navbar.js
@@ -1,19 +1,18 @@
 function navbar() {
   return `<div id="navbar1">
     <div id="left_part">
-        <p>WOMEN</p>
-        <p>MEN</p>
-        <p>KIDS</p>
-        <p>LIFE</p>
+        <p id="womenPage">WOMEN</p>
+        <p id="menPage">MEN</p>
+        <p id="kidPage">KIDS</p>
+        <p id="lifePage">LIFE</p>
     </div>
     <div id="right_part">
-        <P>Signup for Newsletter</P>
-        <P>My account</P>
-        <P>My wishlist ♡</P>
+        <P id="newsletterPage">Signup for Newsletter</P>
+        <P id="accountPage">My account</P>
+        <P id="whishlistPage">My wishlist ♡</P>
         <!-- <i class="fa-thin fa-heart"></i> -->
         <P>Lebanon | English</P>
     </div>
-
 </div>`;
 }
 export default navbar;
--- mensProduct.js
+++ mensProduct.js
@@ -291,3 +291,38 @@ function PriceSort(appendProd, data, container) {
 document.querySelector("#sort").addEventListener("change", function () {
   PriceSort(appendProd, mensData, container);
 });
+
+
+import navbar from "./components/navbar.js"
+
+let navbar_div = document.getElementById("navbar")
+navbar_div.innerHTML = navbar()
+
+import footer from "./components/footer.js"
+
+let footer_div = document.getElementById("footer")
+footer_div.innerHTML = footer()
+
+document.getElementById("womenPage").addEventListener("click",function(){
+  document.location.href = "women.html"
+})
+document.getElementById("menPage").addEventListener("click",function(){
+  window.location.href = "mens.html"
+})
+document.getElementById("kidPage").addEventListener("click",function(){
+  window.location.href = "kids.html"
+})
+document.getElementById("newsletterPage").addEventListener("click",function(){
+  window.location.href = "newsletter.html"
+})
+document.getElementById("accountPage").addEventListener("click",function(){
+  window.location.href = "account.html"
+})
+
+document.getElementById("mytheresa_logo2").addEventListener("click", function(){
+  window.location.href = "myCart.html"
+})
+
+document.getElementById("mytheresa_logo").addEventListener("click", function(){
+  window.location.href = "index.html"
+})
